4e9b7ad60473b7d00cbe588b78eed71b63a5022a
Molly B <103689113+mbruns13@users.noreply.github.com>
2022-08-13 18:41:49 -0500

    adding notes re: potential errors in data, updating test_queries

--- test_queries.sql
+++ test_queries.sql
@@ -9,3 +9,8 @@ SELECT region, state, AVG(median_sale_price) As "Average Median Sale Price"
 FROM county_data
 GROUP BY region, state
 ORDER BY AVG(median_sale_price) DESC;
+
+SELECT region, AVG(median_ppsf) As "Average Median Price Per Square Foot"
+FROM county_data
+GROUP BY region 
+ORDER BY AVG(median_ppsf) DESC;
