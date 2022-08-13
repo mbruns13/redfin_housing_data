5dafbc42094c56b3b9bb47a9557ad4bad3de3fd9
Molly B <103689113+mbruns13@users.noreply.github.com>
2022-08-13 18:27:35 -0500

    updating schema and housing_data code

--- schema.sql
+++ schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE county_data(
 	period_end DATE,
     region VARCHAR,
-    parent_metro_region, VARCHAR 
+    parent_metro_region VARCHAR,
     state VARCHAR,
     property_type VARCHAR,
     inventory INTEGER,
     homes_sold INTEGER,
     median_sale_price DECIMAL,
-    median_ppsf DECIMAL,
+    median_ppsf DECIMAL
 );
