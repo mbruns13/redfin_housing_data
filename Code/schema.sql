f7e3617f7e643514b945ee6afdc08a3e45c3bfca
Molly Bruns <103689113+mbruns13@users.noreply.github.com>
2022-08-17 18:26:04 -0500

    Update schema.sql

--- Code/schema.sql
+++ Code/schema.sql
@@ -1,17 +1,19 @@
 CREATE TABLE county_data(
-	period_end DATE,
+	id SERIAL PRIMARY KEY,
+    period_end DATE,
     year INTEGER,
     county VARCHAR,
     state VARCHAR,
     property_type VARCHAR,
     inventory INTEGER,
     homes_sold INTEGER,
-    median_sale_price DECIMAL,
-    median_ppsf DECIMAL
+    median_sale_price REAL,
+    median_ppsf REAL
 );
 
 CREATE TABLE pop_data(
-	county VARCHAR,
+	id SERIAL PRIMARY KEY,
+    county VARCHAR,
     state VARCHAR,
     pop_estimate_2019 INTEGER,
     pop_estimate_2020 INTEGER,
