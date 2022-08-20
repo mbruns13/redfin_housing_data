d33d25fd18c6144ba6bbf359fbd28bcd2e69411d
Maiterivas <maiterivas09@gmail.com>
2022-08-20 11:30:42 -0500

    Squashed commit of the following:
    commit 16d530d392b7556e7a766a10f39e59acbc0f980f
    Author: Molly B <103689113+mbruns13@users.noreply.github.com>
    Date:   Sat Aug 20 10:15:03 2022 -0500
    
        Update logic.js
    
    commit c2f1204773b8fe0cc2579b50592bdff171ac5189
    Author: Molly B <103689113+mbruns13@users.noreply.github.com>
    Date:   Thu Aug 18 11:40:39 2022 -0500
    
        Update logic.js
    
    commit e48dc44add729144469c49e6dcb7792a4b359157
    Author: Molly B <103689113+mbruns13@users.noreply.github.com>
    Date:   Thu Aug 18 11:35:29 2022 -0500
    
        Update index.html
    
    commit b474c6c256c547cec61091dca5b98668178aee4c
    Author: Molly B <103689113+mbruns13@users.noreply.github.com>
    Date:   Thu Aug 18 11:35:22 2022 -0500
    
        Update logic.js
    
    commit 7d7ba7f3b543fdeccd1a027914ce442396335eb1
    Author: Molly B <103689113+mbruns13@users.noreply.github.com>
    Date:   Thu Aug 18 11:26:59 2022 -0500
    
        Update housing_data.ipynb
    
    commit bf1e321e54bd1fee1ccdbfbc0dbf4e47bd077ae4
    Author: Molly B <103689113+mbruns13@users.noreply.github.com>
    Date:   Thu Aug 18 11:26:54 2022 -0500
    
        Update updated.geojson
    
    commit 6e48db062171336596d4f67eb816798f3b4f6da5
    Author: Molly B <103689113+mbruns13@users.noreply.github.com>
    Date:   Thu Aug 18 01:17:13 2022 -0500
    
        got the county map working
    
    commit ea7cea8e52721aa17987a50fb670151abc9485c2
    Author: Molly B <103689113+mbruns13@users.noreply.github.com>
    Date:   Thu Aug 18 00:20:56 2022 -0500
    
        Update updated.geojson
    
    commit 95e7786cfb13043ecc9567b30092dd4f972f1078
    Author: Molly B <103689113+mbruns13@users.noreply.github.com>
    Date:   Thu Aug 18 00:20:52 2022 -0500
    
        Update housing_data.ipynb
    
    commit e9de2618b7b5a840d204526972b5ecef452e003b
    Author: Molly B <103689113+mbruns13@users.noreply.github.com>
    Date:   Wed Aug 17 23:20:25 2022 -0500
    
        adding yoy values to county geojson data
    
    commit 465f092685c007921e5b6888cdaa6c48e84c96b7
    Author: Molly B <103689113+mbruns13@users.noreply.github.com>
    Date:   Wed Aug 17 21:50:14 2022 -0500
    
        Update test_queries.sql
    
    commit 8a08a1b2ba857dc372d010675dc05ccf79601dab
    Author: Molly B <103689113+mbruns13@users.noreply.github.com>
    Date:   Wed Aug 17 21:50:06 2022 -0500
    
        Update app.py
    
    commit 57c9c33c091e7a0abf0dbbb22464ed139a6ebe4f
    Author: Molly B <103689113+mbruns13@users.noreply.github.com>
    Date:   Wed Aug 17 21:22:47 2022 -0500
    
        Will's help with pivot table for yoy diff columns
    
    commit f66b8a14c0c8696989729b7f81fb1fc6f3dad315
    Author: Molly B <103689113+mbruns13@users.noreply.github.com>
    Date:   Wed Aug 17 21:12:16 2022 -0500
    
        Update test_queries.sql
    
    commit a93658677273d5d64789213f736a73dcfd1f2152
    Author: Molly B <103689113+mbruns13@users.noreply.github.com>
    Date:   Wed Aug 17 21:12:11 2022 -0500
    
        Update app.py
    
    commit f1e17e33f06bec1576cbca5569e558665910f87a
    Author: Maiterivas <maiterivas09@gmail.com>
    Date:   Wed Aug 17 20:12:01 2022 -0500
    
        uploading test chart.js

--- Code/static/js/logic.js
+++ Code/static/js/logic.js
@@ -1,53 +1,132 @@
-url_geojson = "https://raw.githubusercontent.com/mbruns13/project_3_housing_data/Molly/Code/assets/cb_2021_us_county_5m.geojson"
+// used code from: https://leafletjs.com/examples/choropleth/
 
-function createMap(data) {
-    // Create the tile layer that will be the background of our map.
-    var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    });
-    var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-    });
+// colors/legend are not yet updated to reflect our data
 
-    // Create a baseMaps object to hold the lightmap layer.
-    var baseMaps = {
-        "Street Map": street,
-        "Topographic Map": topo
-    };
+var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 4,
+});
 
-    var geojson = L.geoJSON(data, {
+var tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(myMap);
+
+
+var geoData = "https://raw.githubusercontent.com/mbruns13/project_3_housing_data/main/Code/Resources/updated.geojson"
+var geojson;
+
+d3.json(geoData).then(function(data) {
+    console.log(data.features[1]);
+
+    L.geoJson(data, {
         style: {
-            color: "black",
-            fillColor: "none",
-            weight: .5
+            weight: 1,
+            opacity: 1,
+            color: 'none',
+            // dashArray: '3',
+            fillOpacity: 0.6
         }
-    })
-
-    // Create an overlayMaps object to hold the earthquakes layer.
-    var overlayMaps = {
-        "Counties": geojson
-    };
-    // Create the map object with options.
-    var myMap = L.map("map", {
-        center: [37.09, -95.71],
-        zoom: 4,
-        layers: [street, geojson]
-    });
-
-    // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
-    layerControl = L.control.layers(baseMaps, overlayMaps, {
-        collapsed: false
     }).addTo(myMap);
 
-};
+    function getColor(d) {
+        return d > 1000 ? '#800026' :
+            d > 500 ? '#BD0026' :
+            d > 200 ? '#E31A1C' :
+            d > 100 ? '#FC4E2A' :
+            d > 50 ? '#FD8D3C' :
+            d > 20 ? '#FEB24C' :
+            d > 10 ? '#FED976' :
+            'none';
+
+    }
+
+    function style(feature) {
+        return {
+            fillColor: getColor(feature.properties.yoy_2020_diff),
+            weight: 2,
+            opacity: 1,
+            color: 'white',
+            dashArray: '3',
+            fillOpacity: 0.6
+        };
+    }
+
+    function highlightFeature(e) {
+        var layer = e.target;
+
+        layer.setStyle({
+            weight: 5,
+            color: '#666',
+            dashArray: '',
+            fillOpacity: 0.7
+        });
+
+        if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+            layer.bringToFront();
+        }
+        info.update(layer.feature.properties);
+    }
+
+    function resetHighlight(e) {
+        geojson.resetStyle(e.target);
+        info.update();
+    }
+
+    function zoomToFeature(e) {
+        myMap.fitBounds(e.target.getBounds());
+    }
+
+    function onEachFeature(feature, layer) {
+        layer.on({
+            mouseover: highlightFeature,
+            mouseout: resetHighlight,
+            click: zoomToFeature
+        })
+    }
+    var info = L.control();
+
+    info.onAdd = function(myMap) {
+        this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+        this.update();
+        return this._div;
+    };
+
+    // method that we will use to update the control based on feature properties passed
+    info.update = function(props) {
+        this._div.innerHTML = '<h4>Average Median Sale Price - All Residential</h4>' + (props ?
+            '<b>' + props.NAMELSAD + ', ' + props.STATE_NAME + '</b><br />Change in average median sale price from 2019 to 2020: $' + props.yoy_2020_diff + ' ' :
+            'Hover over a county');
+
+    };
+
+    info.addTo(myMap);
+
+    var legend = L.control({ position: 'bottomright' });
+
+    legend.onAdd = function(map) {
+
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+            labels = [];
+
+        // loop through our density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+
+        return div;
+    };
+
+    legend.addTo(myMap);
+
+    geojson = L.geoJson(data, {
+        style: style,
+        onEachFeature: onEachFeature
+    }).addTo(myMap);
 
 
-
-
-d3.json(url_geojson).then(function(data) {
-    console.log(data);
-    //     // console.log(data.features[0])
-
-    createMap(data);
 
 });
