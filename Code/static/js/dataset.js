aad9c949410bae1d57787a60c3935d42db518812
Maiterivas <maiterivas09@gmail.com>
2022-08-24 18:54:33 -0500

    removing unnecessary files

--- Code/static/js/dataset.js
+++
@@ -1,9 +0,0 @@
-const state_url = "http://127.0.0.1:5000/api/v1.0/state_data";
-
-const states = []
-
-d3.json(state_url).then((data) => {
-    for (let i = 0; i < data.length; i++) {
-        states.push((data[i].state))
-    }
-});
